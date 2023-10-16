f4059420eef54aee9998d0019e492af6934b6bd7
12596181452 <fernandes.gentilkadsonjac@gmail.com>
2023-10-16 10:19:47 -0300

    finished static implementation of CC/AD page for SEAC.

--- ccam/people/seac/urls.py
+++ ccam/people/seac/urls.py
@@ -7,4 +7,5 @@ app_name = "seac"
 urlpatterns = [
     path("", views.SeacHomeView.as_view(), name="home"),
     path("knowledge-certificates/", views.SeacViewKnowledgeCertificates.as_view(), name="knowledge_certificates"),
+    path("courses-dispensal/", views.SeacViewCoursesDispensal.as_view(), name="courses-dispensal"),
 ]
