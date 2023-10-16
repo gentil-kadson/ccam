e41d7a5fb735c2df4dc7411ac5f83408954881e9
12596181452 <fernandes.gentilkadsonjac@gmail.com>
2023-10-16 20:53:30 -0300

    rebased seacs files to academics' (makes more sense).

--- ccam/academics/views.py
+++ ccam/academics/views.py
@@ -7,3 +7,19 @@ class KnowledgeCertificateCreateView(TemplateView):
 
 class CourseProgressCreateView(TemplateView):
     template_name = "academics/course_progress_form.html"
+
+
+class SeacViewKnowledgeCertificates(TemplateView):
+    template_name = "academics/seac_academics/seac_view_knowledge_certificates.html"
+
+
+class SeacViewCoursesDispensal(TemplateView):
+    template_name = "academics/seac_academics/seac_view_courses_dispensal.html"
+
+
+class SeacKnowledgeCertificatesStudentDetails(TemplateView):
+    template_name = "academics/seac_academics/knowledge_certifiactes_student_details.html"
+
+
+class SeacCoursesDispensalStudentDetails(TemplateView):
+    template_name = "academics/seac_academics/courses_dispensal_student_details.html"
