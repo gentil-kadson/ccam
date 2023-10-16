b1a44f21b6f094e928b6e86bc471327676411d8b
12596181452 <fernandes.gentilkadsonjac@gmail.com>
2023-10-16 17:14:58 -0300

    feature: renamed knowledge certificate version of students' detail.

--- ccam/people/seac/urls.py
+++ ccam/people/seac/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path("", views.SeacHomeView.as_view(), name="home"),
     path("knowledge-certificates/", views.SeacViewKnowledgeCertificates.as_view(), name="knowledge_certificates"),
     path("courses-dispensal/", views.SeacViewCoursesDispensal.as_view(), name="courses-dispensal"),
-    path("student-details/", views.SeacStudentDetails.as_view(), name="student-details"),
+    path(
+        "knowledge-certificates/student-details/",
+        views.SeacStudentDetails.as_view(),
+        name="knowledge-certificate-student-details",
+    ),
 ]
