87d81f82275b6987aa7ca3f36abfe91caf8f9ccd
Gentil Ályson Jácome Fernandes <gentil.alyson@nees.ufal.br>
2023-10-18 09:01:28 -0300

    feature(directors): adds director form

--- ccam/people/directors/views.py
+++ ccam/people/directors/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class DirectorListView(TemplateView):
     template_name = "directors/directors_list.html"
+
+
+class DirectorCreateView(TemplateView):
+    template_name = "directors/directors_form.html"
