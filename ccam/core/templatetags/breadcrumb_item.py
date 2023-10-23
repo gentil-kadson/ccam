4f5f968a54a406f89dfbc0ffe6ab63178e19b517
Gentil Ályson Jácome Fernandes <gentil.alyson@nees.ufal.br>
2023-10-23 09:38:31 -0300

    feature(core): adds breadcrumb item templatetag

---
+++ ccam/core/templatetags/breadcrumb_item.py
@@ -0,0 +1,15 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag("core/_breadcrumb_item.html")
+def breadcrumb_item(text: str, icon: str, bootstrap_icon=False, active=False, icon_style="outlined", last_item=False):
+    return {
+        "text": text,
+        "icon": icon,
+        "bootstrap_icon": bootstrap_icon,
+        "active": active,
+        "icon_style": icon_style,
+        "last_item": last_item,
+    }
