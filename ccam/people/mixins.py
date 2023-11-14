a95f643062eeacf93aa13684bcbd4ff30991aa92
Gentil Ályson Jácome Fernandes <gentil.alyson@nees.ufal.br>
2023-11-14 08:57:48 -0300

    feature(people): adds betterforms

--- ccam/people/mixins.py
+++
@@ -1,8 +0,0 @@
-from ccam.people.forms import PersonForm
-
-
-class PersonFormMixin:
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context["person_form"] = PersonForm()
-        return context
--- ccam/people/seac/views.py
+++ ccam/people/seac/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 
-from ccam.people.mixins import PersonFormMixin
-
 from .forms import SEACStaffForm
+from .models import SEACStaff
 
 
 class SeacHomeView(TemplateView):
@@ -14,9 +14,16 @@ class SeacStaffListView(TemplateView):
     template_name = "seac/seac_staff_list.html"
 
 
-class SeacStaffCreateView(LoginRequiredMixin, PersonFormMixin, CreateView):
+class SeacStaffCreateView(LoginRequiredMixin, CreateView):
     form_class = SEACStaffForm
     template_name = "seac/seac_staff_form.html"
+    model = SEACStaff
+    success_url = reverse_lazy("people:managers:home")
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        form.instance.updated_by = self.request.user
+        return super().form_valid(form)
 
 
 class SeacStaffDetailView(TemplateView):
