b1c36d7678b69a2572858f09db0819c1e157b312
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-11-14 13:47:06 -0300

    finished knowledge certificate model.

--- ccam/academics/admin.py
+++ ccam/academics/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Course, Subject
+from .models import Course, Subject, KnowledgeCertificate
 
 # Register your models here.
 admin.site.register(Course)
 admin.site.register(Subject)
+admin.site.register(KnowledgeCertificate)
--- ccam/academics/models.py
+++ ccam/academics/models.py
@@ -4,6 +4,8 @@ from django.core.validators import MaxValueValidator
 
 from ccam.core.models import BaseModel
 from ccam.people.coordinators.models import Coordinator
+from ccam.people.students.models import Student
+from ccam.people.seac.models import SEACStaff
 
 
 class Course(BaseModel):
@@ -30,3 +32,14 @@ class Subject(BaseModel):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class KnowledgeCertificate(BaseModel):
+    class Status(models.TextChoices):
+        APPROVED = "AP", _("Aprovado")
+        ANALYZING = "PR", _("Em análise")
+        REJECTED = "ERR", _("Recusado")
+
+    accessed_by = models.ForeignKey(SEACStaff, on_delete=models.CASCADE, related_name="knowledge_certifiacte_assessor")
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="knowledge_certificate_student")
+    subjects = models.ManyToManyField(Subject, related_name="knowledge_certifiace_subjects")
