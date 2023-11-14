e93d59d31f6768478e4322d4e8b02e079d597978
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-11-14 09:47:18 -0300

    finished student db model.

--- ccam/people/students/models.py
+++ ccam/people/students/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+from ccam.core.constants import MAX_LENGTH_PHONE_LINE_FIELD
+from ccam.core.models import BaseModel
+from ccam.people.models import Person
+
+
+class Student(BaseModel):
+    current_grade_semester = models.PositiveSmallIntegerField(
+        verbose_name=_("Ano/Período"), validators=[MaxValueValidator(8)])
+    person = models.OneToOneField(Person, on_delete=models.CASCADE, related_name="person")
+
+    class Meta:
+        verbose_name = _("Student")
+        verbose_name_plural = _("Students")
+
+    def __str__(self) -> str:
+        return f'{self.person.name} - {self.person.registration} - {self.person.phone_number}'
