668f672499316bb7df4d2cee5bfd03e40f10c759
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-11-14 12:57:56 -0300

    subject model implemented.

--- ccam/academics/models.py
+++ ccam/academics/models.py
@@ -21,4 +21,12 @@ class Course(BaseModel):
 
 
 class Subject(BaseModel):
-    pass
+    name = models.CharField(max_length=40, verbose_name=_("Disciplina"))
+    course = models.ManyToManyField(Course, related_name="subject_courses")
+
+    class Meta:
+        verbose_name = _("Subject")
+        verbose_name_plural = _("Subjects")
+
+    def __str__(self):
+        return f'{self.name}'
