e38f3bb53a89c1b1e7af09a6b462bd6c2c906abf
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-11-19 15:18:08 -0300

    student creation implemented.

--- ccam/people/students/apps.py
+++ ccam/people/students/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class StudentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ccam.people.students"
+
+    def ready(self):
+        from ccam.people.students import signals  # noqa: F401
+
+        return super().ready()
--- ccam/people/students/views.py
+++ ccam/people/students/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
+from typing import Any
+from django.views.generic import TemplateView, CreateView, DetailView
+from django.db import models, transaction
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from ccam.people.students.forms import StudentsMultiForm
+from ccam.people.students.models import Student
 
 
 class StudentHomeView(TemplateView):
@@ -9,8 +18,23 @@ class StudentListView(TemplateView):
     template_name = "students/students_list.html"
 
 
-class StudentCreateView(TemplateView):
+class StudentCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     template_name = "students/students_form.html"
+    form_class = StudentsMultiForm
+    model = Student
+    success_url = reverse_lazy("people:managers:home")
+    success_message = _("Estudante criado com sucesso!")
+
+    @transaction.atomic
+    def form_valid(self, form):
+        person = form["person"].save(commit=False)
+        person.created_by = self.request.user
+        person.updated_by = self.request.user
+        student = form["student"].save(commit=False)
+        student.person = person
+        student.created_by = self.request.user
+        student.updated_by = self.request.user
+        return super().form_valid(form)
 
 
 class StudentDetailView(TemplateView):
