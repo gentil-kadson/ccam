91fc2e61fc65f87e404ebaf17162d7bc2cb601fc
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2023-11-22 14:53:01 -0300

    fix(people): removes password from get query on create_person_auth_user signal

--- ccam/people/signals.py
+++ ccam/people/signals.py
@@ -15,8 +15,10 @@ def create_person_auth_user(sender, instance: Person, **kwargs):
     with transaction.atomic():
         cleaned_cpf = clean_cpf(instance.cpf)
         try:
-            auth_user = User.objects.get(username=instance.registration, password=cleaned_cpf)
-            # TODO: Update fields accordingly
+            auth_user = User.objects.get(username=instance.registration)
+            auth_user.username = instance.registration
+            auth_user.password = cleaned_cpf
+            auth_user.save()
         except User.DoesNotExist:
             auth_user = User.objects.create_user(username=instance.registration, password=cleaned_cpf)
             instance.user = auth_user
