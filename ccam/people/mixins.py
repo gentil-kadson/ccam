86fa0acdc8a732a44ff6308d96c1099f0a1f3a8b
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2023-12-05 21:51:55 -0300

    feature(people): adds UserPassesTestMixin mixins for all user groups

--- ccam/people/mixins.py
+++ ccam/people/mixins.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 
-from ccam.core.constants import SEAC_COORDINATOR_GROUP_NAME, STUDENT_GROUP_NAME
+from ccam.core.constants import (
+    COURSE_COORDINATOR_GROUP_NAME,
+    SEAC_COORDINATOR_GROUP_NAME,
+    SEAC_EMPLOYEE_GROUP_NAME,
+    STUDENT_GROUP_NAME,
+    TEACHER_GROUP_NAME,
+)
 
 
 class UserIsStudentTestMixin(UserPassesTestMixin):
@@ -11,3 +17,18 @@ class UserIsStudentTestMixin(UserPassesTestMixin):
 class UserIsSeacCoordinatorTestMixin(UserPassesTestMixin):
     def test_func(self):
         return self.request.user.groups.filter(name=SEAC_COORDINATOR_GROUP_NAME).exists()
+
+
+class UserIsSeacEmployeeTestMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name=SEAC_EMPLOYEE_GROUP_NAME).exists()
+
+
+class UserIsCourseCoordinatorTestMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name=COURSE_COORDINATOR_GROUP_NAME).exists()
+
+
+class UserIsTeacherTestMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name=TEACHER_GROUP_NAME).exists()
