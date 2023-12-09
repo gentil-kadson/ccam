8ba8d2282254d3235626dbccac18389a17717787
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2023-12-09 16:36:02 -0300

    feature(academics): lists teacher committees

--- ccam/academics/teachers/views.py
+++ ccam/academics/teachers/views.py
@@ -5,6 +5,7 @@ from ccam.academics.filters import CommitteeFilterSet
 from ccam.academics.models import Committee
 from ccam.core.views import FilteredListView
 from ccam.people.mixins import UserIsTeacherTestMixin
+from ccam.people.teachers.models import Teacher
 
 
 class TeacherCommitteesListView(LoginRequiredMixin, UserIsTeacherTestMixin, FilteredListView):
@@ -12,3 +13,12 @@ class TeacherCommitteesListView(LoginRequiredMixin, UserIsTeacherTestMixin, Filt
     template_name = "academics/teachers/teacher_committees_filter.html"
     filterset_class = CommitteeFilterSet
     paginate_by = settings.PAGINATE_BY
+    teacher = None
+
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        teacher_pk = kwargs.get("pk")
+        self.teacher = Teacher.objects.get(pk=teacher_pk)
+
+    def get_queryset(self):
+        return Committee.objects.filter(teachers__in=[self.teacher])
