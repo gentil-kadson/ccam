9a8dedcb40898681465e716dd9e75c2e73966cfa
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2023-12-09 14:40:20 -0300

    fix: fixes bug where input animations would play only for one n elements

--- ccam/static/js/file_input.js
+++ ccam/static/js/file_input.js
@@ -1,9 +1,11 @@
-const fileUploadIconElement = document.querySelector("div.ccam-file-input input");
-const fileUploadSvgElement = document.getElementById("file-upload-icon");
-const fileUploadTextContainer = document.getElementById("file-input-text-container");
+const fileUploadIconElement = document.querySelectorAll("div.ccam-file-input input");
+const fileUploadSvgElement = document.querySelectorAll(".file-upload-icon");
+const fileUploadTextContainer = document.querySelectorAll(".file-input-text-container");
 
-fileUploadIconElement.addEventListener("change", (event) => {
-    const textNode = fileUploadTextContainer.childNodes[0]
-    textNode.data = "Carregado"
-    fileUploadSvgElement.innerText = "check_circle"
+fileUploadIconElement.forEach((element, index) => {
+    element.addEventListener("change", (event) => {
+        const textNode = fileUploadTextContainer[index].childNodes[0]
+        textNode.data = "Carregado"
+        fileUploadSvgElement[index].innerText = "check_circle"
+    })
 });
