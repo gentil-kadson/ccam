9f94989f016dfaed5456dbe0f3999241d2fde556
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2023-12-24 11:22:47 -0300

    fix(people): corrects type

--- ccam/people/coordinators/filters.py
+++ ccam/people/coordinators/filters.py
@@ -1,11 +1,15 @@
 import django_filters
+from django.utils.translation import gettext_lazy as _
 
+from ccam.academics.models import Course
 from ccam.people.filters import PersonFilterSet
 
 from .models import Coordinator
 
 
 class CoordinatorsFilterSet(PersonFilterSet, django_filters.FilterSet):
+    course = django_filters.ModelChoiceFilter(label=_("Curso"), queryset=Course.objects.all())
+
     class Meta:
         model = Coordinator
         fields = {"course": ["exact"]} | PersonFilterSet.Meta.fields
--- ccam/people/students/filters.py
+++ ccam/people/students/filters.py
@@ -1,11 +1,15 @@
 import django_filters
+from django.utils.translation import gettext_lazy as _
 
+from ccam.academics.models import Course
 from ccam.people.filters import PersonFilterSet
 
 from .models import Student
 
 
 class StudentFilterSet(PersonFilterSet, django_filters.FilterSet):
+    course = django_filters.ModelChoiceFilter(label=_("Curso"), queryset=Course.objects.all())
+
     class Meta:
         model = Student
         fields = {"current_grade_semester": ["exact"], "course": ["exact"]} | PersonFilterSet.Meta.fields
